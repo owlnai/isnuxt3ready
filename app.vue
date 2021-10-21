966ac3c569de8980637a51b2c5c3f578b7cc3b49
Unai Mengual <20427094+owlnai@users.noreply.github.com>
2021-10-21 18:06:22 +0200

    chore: other design changes

--- app.vue
+++ app.vue
@@ -7,10 +7,10 @@ const { data } = await useAsyncData('modules', () => $fetch('/api/modules'));
     <div class="max-w-6xl mx-auto">
       <section>
         <h1
-          class="text-[60px] font-bold text-nuxt-light text-center leading-normal"
+          class="text-4xl md:text-[60px] font-bold text-nuxt-light text-center leading-normal mt-12"
         >Is Nuxt 3 ready?</h1>
         <p
-          class="text-center text-lg"
+          class="text-center text-md md:text-lg"
         >A community-built compatibility guide of your next Nuxt 3 module</p>
       </section>
       <section class="grid grid-cols-1 lg:grid-cols-3 gap-8 grid-flow-row my-12">
