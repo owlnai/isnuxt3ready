320ecb37ea804e8f692837d9e66ee697bf84a05e
Unai Mengual <20427094+owlnai@users.noreply.github.com>
2021-10-22 18:10:05 +0200

    chore: smart seo

--- components/SeoInjector.vue
+++ components/SeoInjector.vue
@@ -1,22 +1,20 @@
+<script setup>
+const description = "A community-built compatibility guide for Nuxt 3 modules";
+const title = "Is Nuxt 3 Ready?";
+const url = "https://isnuxt3ready.owln.ai/"
+const ogImage = "https://isnuxt3ready.owln.ai/og.png";
+</script>
 <template>
-    <Meta name="description" content="Software Engineer, independent contractor,  OSS translator" />
     <Title>Is Nuxt 3 ready?</Title>
-
-    <!-- Facebook Meta Tags -->
-    <Meta property="og:url" content="https://isnuxt3ready.owln.ai/" />
+    <Meta name="description" :content="desecription" />
+    <Meta property="og:url" :content=url />
     <Meta property="og:type" content="website" />
-    <Meta property="og:title" content="Is Nuxt 3 Ready?" />
-    <Meta
-        property="og:description"
-        content="A community-built compatibility guide for Nuxt 3 modules"
-    />
-    <Meta property="og:image" content="https://isnuxt3ready.owln.ai/og.png" />
+    <Meta property="og:title" :content="title" />
+    <Meta property="og:description" :content="description" />
+    <Meta property="og:image" :content="ogImage" />
     <Meta name="twitter:card" content="summary_large_image" />
-    <Meta property="twitter:domain" content="isnuxt3ready.owln.ai" />
-    <Meta property="twitter:url" content="https://isnuxt3ready.owln.ai/" />
-    <Meta name="twitter:title" content="Is Nuxt 3 Ready?" />
-    <Meta
-        name="twitter:description"
-        content="A community-built compatibility guide for Nuxt 3 modules"
-    />
+    <Meta property="twitter:domain" :content="url.split('/')[2]" />
+    <Meta property="twitter:url" :content="url" />
+    <Meta name="twitter:title" :content="title" />
+    <Meta name="twitter:description" :content="description" />
 </template>
