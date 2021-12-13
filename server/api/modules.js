dd1dd362ee4825ec7aee3558ae4dbe01824bfaf4
Unai Mengual <owlnai@outlook.com>
2021-12-13 12:36:06 +0100

    chore: update

--- server/api/modules.js
+++
@@ -1,8 +0,0 @@
-import data from '../../modules.json';
-export default async (req, res) => {
-  if (req.url.length > 1) {
-    return data.filter(item => item.name == req.url.slice(1))[0];
-  } else {
-    return data;
-  }
-};
